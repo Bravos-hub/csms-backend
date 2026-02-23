f8b8d3f5ce8298fb2c3a277a6fa8d0fa94a3c417
Bravos-hub <bolimi@bugemauniv.ac.ug>
2026-02-23 09:53:03 +0300

    fix(worker): inject kafka module into worker health

--- apps/worker/src/modules/worker-health/worker-health.module.ts
+++ apps/worker/src/modules/worker-health/worker-health.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WorkerHealthController } from './worker-health.controller';
 import { CommandsWorkerModule } from '../commands/commands-worker.module';
+import { KafkaModule } from '../../platform/kafka.module';
 
 @Module({
-    imports: [CommandsWorkerModule],
+    imports: [CommandsWorkerModule, KafkaModule],
     controllers: [WorkerHealthController],
 })
 export class WorkerHealthModule { }
