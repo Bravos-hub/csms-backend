053397e0593ec8ba9cdd3b2fe269e8eae4a95af0
Bravos-hub <bolimi@bugemauniv.ac.ug>
2026-05-14 13:43:19 -0700

    chore: remove artifact files

--- do-refactor.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-// 1. telemetry.types.ts
-let t = fs.readFileSync("apps/api/src/modules/telemetry/telemetry.types.ts", "utf8");
-t = t.replace(
-  `export type VehicleTelemetryProviderAdapter = {`,
-  `export type VehicleTelemetryProviderAdapter = {`
-);
